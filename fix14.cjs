43bdd834737f727e5d47a2139e2c555f63d29b51
sts35047-sketch <sts35047@gmail.com>
2026-08-31 09:28:40 +0530

    Update page title

--- fix14.cjs
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-content = content.replaceAll('React.useState', 'useState');
-fs.writeFileSync('src/App.tsx', content, 'utf8');
--- fix21.cjs
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-
-// Replace font-serif with serif and remove the style attribute
-content = content.replace(/font-serif/g, 'serif');
-content = content.replace(/style=\{\{ fontFamily: '"Playfair Display", Georgia, serif' \}\}/g, '');
-
-fs.writeFileSync('src/App.tsx', content, 'utf8');
-console.log("Font replaced");
